654ec2e8a4e7b32d8999057ed52c92384f6a4f79
wanghui <wanghui_whwx@sina.com>
2015-12-18 12:08:12 +0800

    修改工程名称

--- src/com/woting/mobile/push/monitor/PushListener.java
+++ src/com/woting/mobile/push/monitor/PushListener.java
@@ -6,7 +6,6 @@ import java.util.Timer;
 import com.woting.mobile.push.PushConfig;
 import com.woting.mobile.push.PushSocketServer;
 import com.woting.mobile.push.mem.ReceiveMemory;
-import com.woting.mobile.session.monitor.CleanSessionTask;
 
 public class PushListener extends Thread {
     private static PushConfig pc=null;//推送服务的配置信息
@@ -31,18 +30,16 @@ public class PushListener extends Thread {
             sleep(5000);//多少毫秒后启动任务处理，先让系统的其他启动任务完成，这里设置死为10秒钟
             //初始化内存结构
             ReceiveMemory.getInstance();
-            //启动服务，服务
+            //启动服务
             PushSocketServer pss = new PushSocketServer(pc);
             pss.setDaemon(true);
             pss.start();
             //启动读取线程
-            int i=0;
-            for (;i<pc.getTHREADCOUNT_DEALRECEIVEQUEUE(); i++) {
+            for (int i=0;i<pc.getTHREADCOUNT_DEALRECEIVEQUEUE(); i++) {
                 DealReceivePureQueue drpq = new DealReceivePureQueue(""+i);
                 drpq.setDaemon(true);
                 drpq.start();
             }
-            //启动读取-接收队列-线程
             //启动清理内存服务——垃圾回收
             startCleanMonitor();
         } catch (InterruptedException e) {
