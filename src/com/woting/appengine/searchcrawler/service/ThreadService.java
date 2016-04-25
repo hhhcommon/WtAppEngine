8663cc0afa2521447ffb8c4ab19c60bc845995eb
wangbingqiong <912973694@qq.com>
2016-04-25 15:27:52 +0800

    多平台搜索时去掉节目名称和播放地址重复项

--- src/com/woting/appengine/searchcrawler/service/ThreadService.java
+++ src/com/woting/appengine/searchcrawler/service/ThreadService.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+
 import org.springframework.stereotype.Service;
 import com.woting.appengine.searchcrawler.model.Festival;
 import com.woting.appengine.searchcrawler.model.Station;
@@ -25,36 +26,45 @@ public class ThreadService {
 		Map<String, Object> map = new HashMap<String, Object>();
 		ExecutorService pool = Executors.newFixedThreadPool(3); // 线程池大小4
 		QingTingService qt = new QingTingService(constr); // 创建蜻蜓搜索线程
-//		KaoLaService kl = new KaoLaService(constr); // 创建考拉搜索线程
+		// KaoLaService kl = new KaoLaService(constr); // 创建考拉搜索线程
 		XiMaLaYaService xm = new XiMaLaYaService(constr); // 创建喜马拉雅搜索线程
 		LocalSearch ls = new LocalSearch(constr, resultType, pageType);
-	//	Future<Map<String, Object>> kl_s = pool.submit(kl); // 开启考拉线程
+		// Future<Map<String, Object>> kl_s = pool.submit(kl); // 开启考拉线程
 		Future<Map<String, Object>> qt_s = pool.submit(qt); // 开启蜻蜓线程
 		Future<Map<String, Object>> xm_s = pool.submit(xm); // 开启喜马拉雅线程
 		Future<Map<String, Object>> ls_s = pool.submit(ls); // 开启本地搜索
 		try {
-	//		List<Festival> list_kl_festival = (List<Festival>) kl_s.get().get("KL_F"); // 取出考拉搜索的节目信息
-	//		List<Station> list_kl_station = (List<Station>) kl_s.get().get("KL_S"); // 取出考拉搜索的频道信息
-			List<Festival> list_qt_festival = new ArrayList<Festival>();
-			List<Station> list_qt_station = new ArrayList<Station>();
-			List<Festival> list_xmly_festival = new ArrayList<Festival>();
-			List<Station> list_xmly_station = new ArrayList<Station>();
-			if(qt_s.get()==null){
+			// List<Festival> list_kl_festival = new ArrayList<Festival>(); //
+			// 取出考拉搜索的节目信息
+			// List<Station> list_kl_station = new ArrayList<Station>(); //
+			// 取出考拉搜索的频道信息
+			List<Festival> list_qt_festival = new ArrayList<Festival>(); // 取出蜻蜓搜索的节目信息
+			List<Station> list_qt_station = new ArrayList<Station>(); // 取出蜻蜓搜索的频道信息
+			List<Festival> list_xmly_festival = new ArrayList<Festival>(); // 取出喜马拉雅搜索的节目信息
+			List<Station> list_xmly_station = new ArrayList<Station>(); // 取出喜马拉雅搜索的频道信息
+			List<Map<String, Object>> list_local_festival = new ArrayList<Map<String, Object>>(); // 取出本地搜索的信息
+			/*
+			 * if (kl_s.get() == null) { list_kl_festival = null;
+			 * list_kl_station = null; } else { list_kl_festival =
+			 * (List<Festival>) qt_s.get().get("QT_F"); // 取出蜻蜓搜索的节目信息
+			 * list_kl_station = (List<Station>) qt_s.get().get("QT_S"); //
+			 * 取出蜻蜓搜索的频道信息 }
+			 */
+			if (qt_s.get() == null) {
 				list_qt_festival = null;
 				list_qt_station = null;
-			}else{
+			} else {
 				list_qt_festival = (List<Festival>) qt_s.get().get("QT_F"); // 取出蜻蜓搜索的节目信息
 				list_qt_station = (List<Station>) qt_s.get().get("QT_S"); // 取出蜻蜓搜索的频道信息
 			}
-			if(xm_s.get()==null){
+			if (xm_s.get() == null) {
 				list_xmly_festival = null;
 				list_xmly_station = null;
-			}else{
+			} else {
 				list_xmly_festival = (List<Festival>) xm_s.get().get("XMLY_F"); // 取出喜马拉雅搜索的节目信息
 				list_xmly_station = (List<Station>) xm_s.get().get("XMLY_S"); // 取出喜马拉雅搜索的频道信息
 			}
-			
-			List<Map<String, Object>> list_local_festival = new ArrayList<Map<String, Object>>(); // 取出本地搜索的信息
+
 			if (ls_s.get() == null) {
 				list_local_festival = null;
 			} else {
@@ -62,10 +72,11 @@ public class ThreadService {
 			}
 			List<Station> liststation = new ArrayList<Station>(); // 合并搜索到的频道信息
 			List<Festival> listfestival = new ArrayList<Festival>(); // 合并搜索到的节目信息
-		/*	if (list_kl_station != null)
-				liststation.addAll(list_kl_station);
+			/*
+			if (list_kl_station != null) liststation.addAll(list_kl_station);
 			if (list_kl_festival != null)
-				listfestival.addAll(list_kl_festival);*/
+			listfestival.addAll(list_kl_festival);
+			 */
 			if (list_qt_station != null)
 				liststation.addAll(list_qt_station);
 			if (list_qt_festival != null)
@@ -80,6 +91,26 @@ public class ThreadService {
 					listall.add(map2);
 				}
 			}
+
+			// 去掉重复的节目
+			for (int i = 1; i < listall.size(); i++) {
+				Map<String, Object> map2 = listall.get(i);
+				if (map2 != null) {
+					String name2 = (String) map2.get("ContentName");
+					String url2 = (String) map2.get("ContentPlay");
+					for (int j = 0; j < i; j++) {
+						Map<String, Object> map3 = listall.get(j);
+						if (map3 != null) {
+							String name3 = (String) map3.get("ContentName");
+							String url3 = (String) map3.get("ContentPlay");
+							if (name2.equals(name3) && url2.equals(url3)) {
+								listall.remove(i);
+							}
+						}
+					}
+				}
+			}
+
 			map.put("AllCount", listall.size());
 			map.put("List", listall);
 			map.put("ResultType", resultType);
